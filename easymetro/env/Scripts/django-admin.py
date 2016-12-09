8ee33c28cb634ec0cd102ff69268f4a733a8b57f
Pavel Cerny <cerny.pav@gmail.com>
2016-12-09 18:06:50 +0200

    converted to virtual environment Python

---
+++ easymetro/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\cerny\Documents\fel\DesignInContext\visualstudio-metrobackend\easymetro\easymetro\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
